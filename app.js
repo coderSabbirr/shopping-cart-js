96d0b7c7951fb935c3cb357e78b66ae890f5ad85
Nahid Hasan Sabbir <sabbirnahid5817@gmail.com>
2021-08-20 19:40:02 +0600

    funtion

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-function updateCase(isIncresing){
-    const caseInput = document.getElementById('case-number');
-    const caseNumber =caseInput.value;
+function updateProductNumber(product,price,isIncresing){
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
     if(isIncresing == true){
-        caseInput.value =parseInt(caseNumber) + 1;
+        productNumber =parseInt(productNumber) + 1;
     }
-    else if(caseNumber > 0) {
-        caseInput.value = parseInt(caseNumber) - 1;
+    else if(productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
+    
+ productInput.value = productNumber;
+// update total
+
+    const caseTotal = document.getElementById(product + '-total')
+    caseTotal.innerText = productNumber * price ;
+    
 }
 
+// phone event
+document.getElementById('phone-plus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,true);
+  
+})
+document.getElementById('phone-minus').addEventListener('click',function(){
+    updateProductNumber('phone',1219,false);
+});
 
 
 document.getElementById('case-puls').addEventListener('click',function(){
@@ -16,13 +31,12 @@ document.getElementById('case-puls').addEventListener('click',function(){
     // const caseInput = document.getElementById('case-number');
     // const caseNumber =caseInput.value;
     // caseInput.value =parseInt(caseNumber) + 1;
-    updateCase(true)
-
+    updateProductNumber('case',59,true)
 });
 document.getElementById('case-minus').addEventListener('click',function(){
 // const caseInput = document.getElementById('case-number');
 // const caseNumber = caseInput.value;
 // caseInput.value = parseInt(caseNumber) - 1;
-updateCase(false);
+updateProductNumber('case',59,false);
 
 })
